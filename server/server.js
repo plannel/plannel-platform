1167309300b080174041c18d76e5b1f0cbf955bb
Ron <jexos26@gmail.com>
2021-06-29 11:25:49 +0800

    Deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 import passport from 'passport'
+import path from 'path'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import connectDB from './config/db.js'
 import usersRoutes from './routes/api/users.js'
@@ -29,6 +31,21 @@ config(passport)
 
 // Routes
 app.use('/api/users', usersRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
